142876403e93ebc001623fa8474a6ecf94b6f32c
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-02 21:56:45 +0530

    ..

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-import turtle as t
 import streamlit as st
-
-# Create a drawing
-screen = t.Screen()
-t.circle(50)
-screen.getcanvas().postscript(file="drawing.eps")
-
-# Convert and display the image
-st.image("drawing.eps", caption="Turtle Drawing")
+import turtle as t
+    
+r = 50
+st.write(t.circle(r))
