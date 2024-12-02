715d362e794a04b40b5a0fd969ffefc59962b4b7
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-02 22:40:03 +0530

    chat bot

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 # Single-line text input
-user_input = st.text_input("Enter your name:")
-st.write(f"Hello, {user_input}!")
+user_input = st.text_input("Type your secret message:")
+st.write(f"{user_input}")
