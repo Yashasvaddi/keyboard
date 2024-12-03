deb4cd7ad03a0fdb58db9eddde2dd700c32c1047
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-03 15:17:23 +0530

    updated code

--- test.py
+++ test.py
@@ -4,7 +4,13 @@ import streamlit as st
 st.title("Camera Access Test")
 
 try:
-    cap = cv2.VideoCapture(1)
+    for i in range(5):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            print(f"Camera found at index {i}")
+            break
+
+    cap = cv2.VideoCapture(i)
     if not cap.isOpened():
         st.error("Error: Cannot access the camera. Please check the camera and permissions.")
     else:
